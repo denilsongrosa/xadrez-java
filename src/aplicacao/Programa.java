efa68bc722ed3763d1e727273d5467bbcaefe79e
denilsongrosa <denlson_grosa@hotmail.com>
2020-07-15 00:40:02 -0300

    Començando a implementar Tabuleiro e Peça

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,13 @@
 package aplicacao;
 
-import tabuleiro.Posicao;
+import jogoTabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao posicao = new Posicao(3, 5);
-		System.out.println(posicao);
+		Tabuleiro tabuleiro = new Tabuleiro(8,8);
+		
 	}
 
 }
---
+++ src/jogoTabuleiro/Peca.java
@@ -0,0 +1,17 @@
+package jogoTabuleiro;
+
+public class Peca {
+
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	public Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+}
