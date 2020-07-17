490003eb8623672261d9eb9b60f013cf7e3fd635
denilsongrosa <denlson_grosa@hotmail.com>
2020-07-17 00:38:42 -0300

    Alteraçao nas cores do tabuleiro

--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -1,10 +1,33 @@
 package aplicacao;
 
+import xadrez.Cor;
 import xadrez.PecaXadrez;
 
 public class UI {
 	
 	//Classe utilizada para imprimir peças e tabuleiro
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+	
+	//Cores do texto
+	public  static  final  String  ANSI_RESET  =  "\u001B[0m" ;
+	public  static  final  String  ANSI_BLACK  =  "\u001B[30m" ;
+	public  static  final  String  ANSI_RED  =  "\u001B[31m" ;
+	public  static  final  String  ANSI_GREEN  =  "\u001B[32m" ;
+	public  static  final  String  ANSI_YELLOW  =  "\u001B[33m" ;
+	public  static  final  String  ANSI_BLUE  =  "\u001B[34m" ;
+	public  static  final  String  ANSI_PURPLE  =  "\u001B[35m" ;
+	public  static  final  String  ANSI_CYAN  =  "\u001B[36m" ;
+	public  static  final  String  ANSI_WHITE  =  "\u001B[37m" ;
+
+	//Cores do fundo
+	public  static  final  String  ANSI_BLACK_BACKGROUND  =  "\u001B[40m" ;
+	public  static  final  String  ANSI_RED_BACKGROUND  =  "\u001B[41m" ;
+	public  static  final  String  ANSI_GREEN_BACKGROUND  =  "\u001B[42m" ;
+	public  static  final  String  ANSI_YELLOW_BACKGROUND  =  "\u001B[43m " ;
+	public  static  final  String  ANSI_BLUE_BACKGROUND  =  "\u001B[44m" ;
+	public  static  final  String  ANSI_PURPLE_BACKGROUND  =  "\u001B[45m" ;
+	public  static  final  String  ANSI_CYAN_BACKGROUND  =  "\u001B[46m" ;
+	public  static  final  String  ANSI_WHITE_BACKGROUND  =  "\u001B[47m" ;
 	
 	
 	public static void imprimirTabuleiro(PecaXadrez[][] pecas) {
@@ -19,11 +42,19 @@ public class UI {
 	}
 	
 	private static void imprimirPeca(PecaXadrez peca) {
-		if (peca == null) {
-			System.out.print("-"); //impressão caso não tenha peça na posição atual
-		}else {
-			System.out.print(peca); //imprimi a peça caso tenha alguma na posição
-		}
-		System.out.print(" ");
+    	if (peca == null) {
+            System.out.print("-"); //impressão caso não tenha peça na posição atual
+        }
+        else {
+        	//o if abaixo verifica a cor de impressão da peça, se a peça tem a cor branca, imprime no branco 
+        	//se tem a cor preta imprimi no amarelo por causa do fundo do terminal
+            if (peca.getCor() == Cor.WHITE) {
+                System.out.print(ANSI_WHITE + peca + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + peca + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
 }
